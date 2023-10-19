9de5af995d8fdb8d573534b3cfc642313798b733
github@zbay.llc <github@zbay.llc>
2023-10-19 16:35:37 +0200

    chore: build iphoneos binary

--- build-binaries/ios.sh
+++ build-binaries/ios.sh
@@ -1,6 +1,7 @@
 NODEJS_HEADERS_DIR="$( cd ../../ && cd nodedir/ios/libnode/ && pwd )"
 
 ARCH=$1
+PLATFORM=$2
 
 # Build for arm64 by default
 if [ -z $ARCH ]
@@ -9,18 +10,24 @@ then
 fi
 
 # Choose proper target basing on architecture we build against
-if [ $ARCH == "arm64" ]
+if [ $PLATFORM == "iphoneos" ]
 then
+  DEFINES="OS=ios target_arch=arm64 iossim=false"
   TARGET="aarch64-apple-ios"
+elif [ $ARCH == "arm64" ]
+then
+  DEFINES="OS=ios target_arch=arm64 iossim=true"
+  TARGET="aarch64-apple-ios-sim"
 elif [ $ARCH == "x64" ]
 then
+  DEFINES="OS=ios target_arch=x64 iossim=true"
   TARGET="x86_64-apple-ios"
 else
   echo "ERR: Provided architecture $ARCH is not supported."
   exit 1
 fi
 
-GYP_DEFINES="OS=ios" \
+GYP_DEFINES="$DEFINES" \ 
 CARGO_BUILD_TARGET="$TARGET" \
 npm_config_nodedir="$NODEJS_HEADERS_DIR" \
 npm_config_platform="ios" \
--- build.sh
+++ build.sh
@@ -1,56 +1,60 @@
 PROJECT_DIR="$( pwd )"
 
 OS=$1
-VARIANT=$2
+ARCHS=$2
+PACKAGES=$3
 
-# Handle building universal binary for iOS
-if [ $OS == "ios" ] && [ $VARIANT == "universal" ]
-then
-  ARCHS=("x64" "arm64")
-else
-  ARCHS=($VARIANT)
-fi
+#print packages
+echo "Building packages: $PACKAGES"
 
 # Delete previous artifacts
 rm -rf deps/$OS
 mkdir deps/$OS
 
 # Prepare proper addon.gypi for node-gyp-build
+rm -rf node_modules/node-gyp
+
 if [ $OS == "android" ]
 then
-  rm -rf node_modules/node-gyp
   cp -rf node_modules/nodejs-mobile-gyp node_modules/node-gyp
 elif [ $OS == "ios" ]
 then
-  rm -rf node_modules/node-gyp
   npm i
   npm run apply-mobile-addon-gypi
 else
-  rm -rf node_modules/node-gyp
   npm i
 fi
 
-for ARCH in ${ARCHS[@]}; do
+if [ $OS == "ios" ]
+then
+  ARCHS=("arm64" "x64")
+  BUILD_IOS=true
+else
+  ARCHS=("$ARCH")
+  BUILD_IOS=false
+fi
 
-  # Build binaries
-  for package in ${3//,/ }; do
-  echo "Building $OS $ARCH shared library for $package"
-  rm -rf $PROJECT_DIR/node_modules/$package/build
-  cd $PROJECT_DIR/node_modules/$package && $PROJECT_DIR/build-binaries/$OS.sh $ARCH
+for package in ${PACKAGES//,/ }; do
+  for arch in ${ARCHS[@]}; do
+
+    # Build package
+    echo "Building $OS $arch shared library for $package"
+    rm -rf $PROJECT_DIR/node_modules/$package/build
+    cd $PROJECT_DIR/node_modules/$package && $PROJECT_DIR/build-binaries/$OS.sh $arch
+
+    # Generate .framework for iOS
+    if [ $BUILD_IOS == true ]
+    then      
+      node $PROJECT_DIR/ios-frameworks/ios-create-plists-and-dlopen-override.js $PROJECT_DIR/node_modules $arch
+    fi
   done
-
-  # Generate ios .frameworks
-  if [ $OS == 'ios' ]
-  then
-      node $PROJECT_DIR/ios-frameworks/ios-create-plists-and-dlopen-override.js $PROJECT_DIR/node_modules $ARCH
-  fi
 done
 
-# (Optionally) merge universal binary for iOS
-if [ $OS == "ios" ] && [ $VARIANT == "universal" ]
+# (iOS) Merge universal binary for simulator
+if [ $BUILD_IOS == true ]
 then
-  for package in ${3//,/ }; do
-    DEST=$PROJECT_DIR/deps/$OS/universal/$package/$package.framework/
+  for package in ${PACKAGES//,/ }; do
+    DEST=$PROJECT_DIR/deps/$OS/ios-arm64_x86_64-simulator/$package/$package.framework/
 
     for arch in ${ARCHS[@]}; do
       mkdir -p $DEST
@@ -66,4 +70,16 @@ then
     done
 
   done
+fi
+
+# (iOS) Build .framework for device
+if [ $BUILD_IOS == true ]
+then
+  for package in ${PACKAGES//,/ }; do
+    echo "Building iOS device shared library for $package"
+    rm -rf $PROJECT_DIR/node_modules/$package/build
+    cd $PROJECT_DIR/node_modules/$package && $PROJECT_DIR/build-binaries/ios.sh arm64 iphoneos
+  done
+
+  node $PROJECT_DIR/ios-frameworks/ios-create-plists-and-dlopen-override.js $PROJECT_DIR/node_modules ios-arm64
 fi
